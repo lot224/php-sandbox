bf4af7b44d82843137378766d9b4368142f50e46
Dave Gardyasz <evad@lot224.net>
2021-12-29 15:48:38 -0500

    Init2

---
+++ server/src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Seed Server</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">PHP Seed Server</a>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <h3>Welcome, this is a basic PHP site.</h3>
+    </div>
+  </body>
+
+</html>
